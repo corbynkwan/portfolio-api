770088819b5d72d048a8bafc1a6dd2f1a48be1e5
Corbyn Kwan <corbynkwan@gmail.com>
2020-11-13 13:12:30 +0800

    'init'

--- app.js
+++ app.js
@@ -1,55 +1,7 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const port = 3030;
-const sendGrid = require('@sendGrid/mail');
-
-
 const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.use(bodyParser.json());
-
-app.use(cors());
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-
-
-app.post('/api/email', (req, res, next) => {
-
-    console.log(req.body);
-
-    sendGrid.setApiKey('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');
-    const msg = {
-        to: 'corbynkwan@gmail.com',
-        from: req.body.email,
-        subject: 'Website Contact',
-        text: req.body.message
-    }
-
-    sendGrid.send(msg)
-        .then(result => {
-
-            res.status(200).json({
-                success: true
-            });
-
-        })
-        .catch(err => {
-
-            console.log('error: ', err);
-            res.status(401).json({
-                success: false
-            });
-
-        });
-});
-
-// || port
-app.listen(port = process.env.PORT || 80);
+app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
