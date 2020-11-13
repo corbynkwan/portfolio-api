047e948fc82d848519e239713dc50581aa51d47f
Corbyn Kwan <corbynkwan@gmail.com>
2020-11-13 13:10:34 +0800

    'init'

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = 303   0;
+const port = 3030;
 const sendGrid = require('@sendGrid/mail');
 
 
