3bbd366c90022bdc9f7d36a3ef6f625fa381242c
Corbyn Kwan <corbynkwan@gmail.com>
2020-11-13 13:47:10 +0800

    'init'

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = 3000;
-
+const sendGrid = require('@sendGrid/mail');
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
