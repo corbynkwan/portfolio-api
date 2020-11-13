ce5c8661843b08ee1ecc32b5ec2b951466b14211
Corbyn Kwan <corbynkwan@gmail.com>
2020-11-13 13:22:59 +0800

    'init'

--- app.js
+++ app.js
@@ -25,35 +25,7 @@ app.get('/api', (req, res, next) => {
     res.send('API Status: I\'m awesome')
 });
 
-app.post('/api/email', (req, res, next) => {
 
-    console.log(req.body);
-
-    sendGrid.setApiKey('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');
-    const msg = {
-        to: 'corbynkwan@gmail.com',
-        from: req.body.email,
-        subject: 'Website Contact',
-        text: req.body.message
-    }
-
-    sendGrid.send(msg)
-        .then(result => {
-
-            res.status(200).json({
-                success: true
-            });
-
-        })
-        .catch(err => {
-
-            console.log('error: ', err);
-            res.status(401).json({
-                success: false
-            });
-
-        });
-});
 
 // || port
 console.log(process.env.PORT);
